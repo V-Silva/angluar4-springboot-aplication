2561d49e53e712d57da794d540a096b50fc156a6
adrianaden <a.adendrata@gmail.com>
2017-09-07 23:23:08 +0700

    modify jwt util

--- backend/src/main/java/com/adrianaden/app/util/JWTUtil.java
+++ backend/src/main/java/com/adrianaden/app/util/JWTUtil.java
@@ -11,10 +11,10 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-public class JwtUtil {
+public class JWTUtil {
 	String secretKey = "abc123";
 	
-	private String createJWT(String id, String issuer, String subject, long ttlMillis) {
+	public String createJWT(String id, String issuer, String subject, long ttlMillis) {
 
 		// The JWT signature algorithm we will be using to sign the token
 		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
@@ -27,7 +27,11 @@ public class JwtUtil {
 		Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
 		// Let's set the JWT Claims
-		JwtBuilder builder = Jwts.builder().setId(id).setIssuedAt(now).setSubject(subject).setIssuer(issuer)
+		JwtBuilder builder = Jwts.builder()
+				.setId(id)
+				.setIssuedAt(now)
+				.setSubject(subject)
+				.setIssuer(issuer)
 				.signWith(signatureAlgorithm, signingKey);
 
 		// if it has been specified, let's add the expiration
@@ -42,11 +46,17 @@ public class JwtUtil {
 	}
 	
 	//Sample method to validate and read the JWT
-	private void parseJWT(String jwt) {
+	private Claims parseJWT(String jwt) {
 	 
 	    //This line will throw an exception if it is not a signed JWS (as expected)
 	    Claims claims = Jwts.parser()         
 	       .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
 	       .parseClaimsJws(jwt).getBody();
+	    
+	    return claims;
+	}
+	
+	public boolean isValidJWT(String jwt) {
+		return this.parseJWT(jwt) != null;
 	}
 }
